9276c16e5f8976577d4299db64a9b4a55c8b531c
unknown <sohail.iqbal@digitify.com>
2023-09-16 16:07:00 +0500

    update cros

--- src/infrastructure/web_server/express/index.js
+++ src/infrastructure/web_server/express/index.js
@@ -15,9 +15,19 @@ const { ROLES } = require('../../../database/enums');
 
 const app = express();
 
+// CORS middleware
+const allowCrossDomain = (req, res, next) => {
+  res.header(`Access-Control-Allow-Origin`, `example.com`);
+  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+  next();
+};
+
+//...
 function start(port) {
   app.use(loggingMiddleware);
-  app.use(cors());
+  // app.use(cors());
+  app.use(allowCrossDomain);
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   app.use(cookieParser());
